e966de78e24bc785c7463cb3b35f6f3cb11ef554
TomasVotruba <tomas.vot@gmail.com>
2016-08-22 11:38:58 +0200

    decrease complexity

--- src/Console/Command/RunCommand.php
+++ src/Console/Command/RunCommand.php
@@ -7,6 +7,7 @@
 
 namespace Symplify\PHP7_CodeSniffer\Console\Command;
 
+use Exception;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -77,18 +78,11 @@ final class RunCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         try {
-            $this->application->runCommand(new RunApplicationCommand(
-                $input->getArgument('source'),
-                $input->getOption('standards'),
-                $input->getOption('sniffs'),
-                $input->getOption('exclude-sniffs'),
-                $input->getOption('fix')
-            ));
+            $this->application->runCommand($this->createCommandFromInput($input));
 
             if ($this->errorDataCollector->getErrorCount()) {
                 $this->infoMessagePrinter->printFoundErrorsStatus($input->getOption('fix'));
-
-                return ExitCode::ERROR;
+                throw new Exception();
             }
 
             $this->codeSnifferStyle->success(
@@ -105,11 +99,17 @@ final class RunCommand extends Command
 
     private function addArrayOption(string $name, string $description)
     {
-        $this->addOption(
-            $name,
-            null,
-            InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY,
-            $description
+        $this->addOption($name, null, InputOption::VALUE_REQUIRED | InputOption::VALUE_IS_ARRAY, $description);
+    }
+
+    private function createCommandFromInput(InputInterface $input) : RunApplicationCommand
+    {
+        return new RunApplicationCommand(
+            $input->getArgument('source'),
+            $input->getOption('standards'),
+            $input->getOption('sniffs'),
+            $input->getOption('exclude-sniffs'),
+            $input->getOption('fix')
         );
     }
 }
